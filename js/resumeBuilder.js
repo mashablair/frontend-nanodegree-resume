adbf5fa5fcd6b84eccf0ed3632bf8300ada83604
Maria Blair <maria.klachko@gmail.com>
2016-07-25 12:46:55 -0700

    Delete frontend-nanodegree-resume folder and commit changes in index.html and js/helper.js files.  Practiced my newly ackquired JS and jQuery skills! :)

---
+++ js/resumeBuilder.js
@@ -0,0 +1,16 @@
+$("#main").append("Maria Blair");
+
+var firstName = "Maria";
+var age = 35;
+var awesomeThoughts = "I am Maria and I am AWESOME! :)";
+console.log(firstName);
+console.log(age);
+console.log(awesomeThoughts);
+
+var fun = ("FUN");
+
+var funThoughts = awesomeThoughts.replace("AWESOME", fun);
+console.log(funThoughts);
+
+var sophieThoughts = "Sophie is AWESOME".replace("AWESOME", "adorable");
+console.log(sophieThoughts);
