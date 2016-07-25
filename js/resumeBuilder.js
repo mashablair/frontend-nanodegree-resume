22a80599824a7a7033e3dfb65256fa58d5411f47
Maria Blair <maria.klachko@gmail.com>
2016-07-25 15:01:07 -0700

    more changes to resumeBuilder.js before learning Arrays

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,17 +1,26 @@
-$("#main").append("Maria Blair");
+// $("#main").append("Maria Blair");
 
-var firstName = "Maria";
-var age = 35;
-var awesomeThoughts = "I am Maria and I am AWESOME! :)";
-console.log(firstName);
-console.log(age);
-console.log(awesomeThoughts);
+// var firstName = "Maria";
+// var age = 35;
+// var awesomeThoughts = "I am Maria and I am AWESOME! :)";
+// console.log(firstName);
+// console.log(age);
+// console.log(awesomeThoughts);
 
-var fun = ("FUN");
+// var fun = ("FUN");
 
-var funThoughts = awesomeThoughts.replace("AWESOME", fun);
-console.log(funThoughts);
+// var funThoughts = awesomeThoughts.replace("AWESOME", fun);
+// console.log(funThoughts);
 
-var sophieThoughts = "Sophie is AWESOME".replace("AWESOME", "adorable");
-console.log(sophieThoughts);
+// var sophieThoughts = "Sophie is AWESOME".replace("AWESOME", "adorable");
+// console.log(sophieThoughts);
 
+var name = "Maria Blair";
+var role = "Front-End Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
