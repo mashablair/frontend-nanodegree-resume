220a3b4e16f68c7a747feeab20e421b7171fefa0
Maria Blair <maria.klachko@gmail.com>
2016-07-25 17:17:51 -0700

    more changes to resumeBuilder.js including JS objects practice

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -24,3 +24,25 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
+
+
+var bio = {
+    "name": "Maria Blair",
+    "role": "Front-End Web Developer",
+    //object:
+    "contact info": {
+        "email": "mashablair@gmail.com",
+        "cell": "805-636-6461",
+        "github": "mashablair",
+        "location": "Santa Barbara, CA",
+        "twitter": "mashablair"
+    },
+    //end of object
+    "picture": "images/fry.jpg",
+    "welcome message": "Welcome to my online resume site!",
+    "skills": [
+        "programming", "HTML", "CSS", "JavaScript", "Bootstrap", "Sublime Text"
+    ],
+};
+$("#main").append(bio);
+
