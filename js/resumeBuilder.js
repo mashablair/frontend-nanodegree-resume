d6ae5c56dfdb6bf0b35d1058990d84da13c44859
Maria Blair <maria.klachko@gmail.com>
2016-07-27 13:18:23 -0700

    Added for-in loop to resumeBuilder.js file for work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -94,6 +94,18 @@ var work = {
     ]
 };
 
+// for-in loop for jobs
+for (job in work.jobs) {
+    $("workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+    $(".work-entry:last").append(
+        formattedEmployerTitle);
+}
+
 //projects object
 var projects = {
     "projects": [
@@ -111,4 +123,3 @@ var projects = {
         }
     ]
 };
-
