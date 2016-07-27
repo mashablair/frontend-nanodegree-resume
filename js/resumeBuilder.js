56b22b96b51fe42839f69e0d5bc07006e557dcd9
Maria Blair <maria.klachko@gmail.com>
2016-07-27 13:28:20 -0700

    Edited for-in loop for work iterating through Dates and Description for the jobs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -96,14 +96,20 @@ var work = {
 
 // for-in loop for jobs
 for (job in work.jobs) {
+    // create new div for work experience
     $("workExperience").append(HTMLworkStart);
-
+    //concat employer and title
     var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
     var formattedTitle = HTMLworkTitle.replace("%data", work.jobs[job].title);
     var formattedEmployerTitle = formattedEmployer + formattedTitle;
-
     $(".work-entry:last").append(
         formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
 }
 
 //projects object
